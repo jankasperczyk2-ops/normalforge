bc361362f799f069b873b349d15c7812a6d471b2
jankasperczyk2-ops <jankasperczyk2@gmail.com>
2026-03-02 23:24:53 +0100

    Update standalone script with GPL header

--- normalforge.py
+++ normalforge.py
@@ -1,11 +1,30 @@
+# NormalForge — One-click bevel + custom normals workflow for game-ready meshes
+# Copyright (C) 2024 3DC Jan Kasperczyk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 bl_info = {
     "name": "NormalForge",
-    "author": "Your Name",
+    "author": "Jan Kasperczyk",
     "version": (3, 0, 0),
     "blender": (4, 0, 0),
     "location": "View3D > Sidebar > NormalForge",
-    "description": "One-click bevel + custom normals workflow for game-ready meshes",
+    "description": "One-click bevel + custom normals workflow for Nanite-ready meshes",
+    "doc_url": "https://github.com/jankasperczyk2-ops/normalforge",
+    "tracker_url": "https://github.com/jankasperczyk2-ops/normalforge/issues",
     "category": "Mesh",
+    "license": "GPL-3.0-or-later",
 }
 
 import bpy
